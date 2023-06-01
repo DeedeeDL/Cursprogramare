c55e81c464bf8091ab14c0c4deb6181b6ec233ec
DeedeeDL <diana_girl_89@yahoo.com>
2023-06-01 16:23:33 +0300

    Corectare_problema_scadere

--- Teme pasul 1/Problema_scadere/main.cpp
+++ Teme pasul 1/Problema_scadere/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main()
-{
+{/*Am realizat un program care sa afiseze operatia de scadere dintre cele doua variabile x si y */
     int x,y;
     cin >> x >>y;
 
-    cout << x+y << endl;
+    cout << x-y << endl;
     return 0;
 }
