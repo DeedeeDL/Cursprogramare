af67a0237363077248fc5ec6e0665f2b43f651e1
DeedeeDL <diana_girl_89@yahoo.com>
2023-06-01 20:20:12 +0300

    modificari variabile

--- Teme pasul 1/Pblema_pagini_citite/main.cpp
+++ Teme pasul 1/Pblema_pagini_citite/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 //* Crearea unui program din care sa reiasa numarul total de pagini citite (n) *//
 int main()
 {
-int f, b, n ; //* 3 variabile declarate, reprezentand fete, baieti si nr total de pagini citite *//
-    cin >> f >> b >> n;
-    cout << (f*3+b*2)*n << endl;
+int fete, baieti, nrpagini ; //* 3 variabile declarate, reprezentand fete, baieti si nr total de pagini citite *//
+    cin >> fete >> baieti >> nrpagini;
+    cout << (fete*3+baieti*2)*nrpagini << endl;
     return 0;
 }
